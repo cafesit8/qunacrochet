88d1f3045795b7860bc5881001bcff235a7bcc70
leliasgomer <leliasgomer@gmail.com>
2024-09-14 15:07:09 -0500

    create footer section

---
+++ src/app/sections/footer/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="w-full bg-custom-black text-white px-4">
+      <div>
+        <p className="text-center py-10">© 2024 Qunacrochet. Todos los derechos reservados</p>
+      </div>
+    </footer>
+  )
+}
