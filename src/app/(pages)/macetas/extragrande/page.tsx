8cc20d9774720709fa58741ab402053b69268cff
leliasgomer <leliasgomer@gmail.com>
2024-09-27 21:06:11 -0500

    change name

--- src/app/(pages)/macetas/extragrande/page.tsx
+++ src/app/(pages)/macetas/extragrande/page.tsx
@@ -5,7 +5,7 @@ export default function MacetaExtraGrandePage () {
   return (
     <div className="w-full bg-white text-custom-black py-10">
       <div className="max-w-[1300px] m-auto px-4">
-        <h1 className="md:text-6xl text-4xl text-center font-playfair">Macetas Pequeñas</h1>
+        <h1 className="md:text-6xl text-4xl text-center font-playfair">Macetas Extra Grande</h1>
         <p className="text-center md:text-base text-sm mt-2">Para la descripción general que va antes de los productos:
           Las flores más bellas en macetas eternas, perfectas para decorar todos los espacios de la casa, oficina, entre otros 🤗
           La maceta va dentro de una caja decorada por dentro con papel seda, listas para regalar 🎁
